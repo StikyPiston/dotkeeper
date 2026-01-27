df9528afe69a243f3e6091293545ef8ec0bddab5
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-27 12:28:06 +0200

    feat: list subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use clap::Parser;
-use clap::Subcommand;
+use clap::{Parser, Subcommand};
+use std::fs;
+use std::path::PathBuf;
 
 /// dotkeeper — manage dotfile keeps
 #[derive(Parser)]
@@ -35,12 +36,45 @@ enum Commands {
     },
 }
 
+fn dotkeep_dir() -> PathBuf {
+    let home = std::env::var("HOME").expect("HOME not set");
+    PathBuf::from(home).join(".dotkeep")
+}
+
+fn list_keeps() -> std::io::Result<()> {
+    let dir = dotkeep_dir();
+
+    if !dir.exists() {
+        println!("{} does not exist, please create it.", dir.display());
+        return Ok(());
+    }
+
+    println!("Available keeps:"); // TODO:: Add nerd font icon
+
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
+                if !name.starts_with(".") {
+                    println!("- {}", name)
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
     let cli = Cli::parse();
 
     match cli.command {
         Commands::List => {
-            println!("list not implemented yet");
+            if let Err(e) = list_keeps() {
+                eprintln!("Error: {}", e);
+            }
         }
         Commands::Status => {
             println!("status not implemented yet");
@@ -54,5 +88,6 @@ fn main() {
         Commands::Fetch { url } => {
             println!("fetch keep from: {url}");
         }
+        _ => {}
     }
 }
