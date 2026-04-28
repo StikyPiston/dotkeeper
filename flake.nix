6ef8764ec6f9194a9ded6330f1ff1f4a8b188e00
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-28 12:39:14 +0200

    chore(flake.nix): bump version

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
         packages.dotkeeper = pkgs.buildGoModule {
           pname = "dotkeeper";
-          version = "2026.03.22-a";
+          version = "2026.04.28-a";
 
           src = self;
 
